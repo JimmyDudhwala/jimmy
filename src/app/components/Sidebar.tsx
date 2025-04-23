7b8981864c0a3c19cc0bd2f8184f924d225df648
JimmyDudhwala <jimmydudhwala25@gmail.com>
2025-04-23 12:50:08 +0530

    Update Sidebar component to adjust margin for improved layout on medium screens

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -38,7 +38,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
             exit={{ x: -2000, borderTopRightRadius: "350px", borderBottomRightRadius: "350px" }}
             className={`h-screen z-90  lg:w-[50%] w-[98%] bg-[#0F3443] absolute top-0 left-0 shadow-[60px_0_100px_rgba(0,0,0,0.4)] overflow-hidden`}
         >
-            <div className='h-full w-full mt-[40px] bg-[#0F3443] flex flex-col justify-center lg:items-start items-center'>
+            <div className='h-full w-full mt-[40px] md:mt-0 bg-[#0F3443] flex flex-col justify-center lg:items-start items-center'>
             {items.map((item) => (
                 <div
                 key={item.id}
